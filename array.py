ef7342aa49c8c4cd55d70bfb222b6ec4a0f3f964
Niraj Adhikari <nrjadkry@gmail.com>
2020-07-02 14:41:32 +0545

    Array file is added

---
+++ array.py
@@ -0,0 +1,18 @@
+numbers=[1,2,3,4,5,6,7,8,9]
+
+print(numbers)
+print(numbers[0])
+print(numbers[8])
+print(len(numbers))
+
+print(numbers[-1]) #prints the last number
+print(numbers[-len(numbers)])
+
+print(numbers[:2]) #prints the first 2 numbers
+print(numbers[0:8])
+print(numbers[-3:9])
+
+print(numbers[5:]) #prints from index no 5 to last
+
+print(numbers[0:9:2]) #prints from 1st to 9th skipping by 2
+
